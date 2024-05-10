ad302f9addd049fb9af50d748668a3d531421cb2
ChulSungByeon <168974977+ChulSungByeon@users.noreply.github.com>
2024-05-10 16:32:23 +0900

    Create 00025_원소들의 곱과 합.java

---
+++ 00025_원소들의 곱과 합.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int solution(int[] num_list) {
+        int answer = 0;
+        int A = 1;
+        int B = 0;
+        
+        for(int i=0; i< num_list.length; i++){
+            A *= num_list[i];
+            B += num_list[i];
+        }
+        
+        if(A < Math.pow(B,2)){
+            answer = 1;
+        }
+        
+        return answer;
+    }
+}
